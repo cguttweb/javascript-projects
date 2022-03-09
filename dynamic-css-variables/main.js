67d053147402476550fb45c10bb118dc8fc3a42e
Chloe <cguttweb@pm.me>
2022-03-09 21:20:14 +0000

    working on styling & starting js

--- dynamic-css-variables/main.js
+++ dynamic-css-variables/main.js
@@ -3,19 +3,25 @@ import './style.css'
 const loginBtn = document.getElementById('login')
 const cancelBtn = document.getElementById('cancel')
 
-const username = document.getElementById('username').value
-const password = document.getElementById('password').value
+let username = document.getElementById('username').value
+let password = document.getElementById('password').value
+
+let errorMessage = document.querySelector('.message')
 
 function validateInput() {
   if (username !== 'testuser') {
-    console.log('username doesnt match')
+    console.log('username does not match')
   }
   if (password !== 'mypassword') {
-    console.log('password doesnt match')
+    console.log('password does not match')
   }
 }
 
 loginBtn.addEventListener('click', e => {
   e.preventDefault()
   validateInput()
+})
+
+cancelBtn.addEventListener('click', e => {
+  e.preventDefault()
 })
