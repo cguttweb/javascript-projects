c026b173acd63824ce87ad394ebfa4d34ef3a365
Chloe <cguttweb@pm.me>
2022-07-26 08:22:58 +0100

    attempting to generate a random number

--- random-number-generator/main.js
+++ random-number-generator/main.js
@@ -1,16 +1,18 @@
 import './style.css'
 
-let minNum = document.querySelector('#min').value
-let maxNum = document.querySelector('#max').value
+let minNum = document.querySelector('#min')
+let maxNum = document.querySelector('#max')
 let genButton = document.querySelector('#genNumber')
 let numberGenerated = document.querySelector('#numberGenerated')
 
-function generateNum(min, max){
-  return 
+function generateRandomNum(min, max){
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-genButton.addEventListener('click', () => {
-  generateNum()
-  numberGenerated.innerHTML = `Your number is between ${minNum} and ${maxNum}`
-})
 
+genButton.addEventListener('click', () => {
+  generateRandomNum(minNum, maxNum)
+  numberGenerated.innerHTML = `Your number is ${generateRandomNum()}`
+})
